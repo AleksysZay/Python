8dc5b36ee3b0c1deefb3ca2ef5a5d1c9b17a3a63
Alexey Zayats <alekseyzayats.h8@gmail.com>
2022-09-04 11:09:58 +0300

    Basic syntax in Python

---
+++ Lesson_2/basic_syntax.py
@@ -0,0 +1,10 @@
+#multiple instructions on one line
+a = 1; b = 2; print(a + b)
+
+#one instraction in multiple lines
+if (a != b or
+    b == 1):
+    print(b - a)
+
+#compound instruction
+if b > a: print('+')
